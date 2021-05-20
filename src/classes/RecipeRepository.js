c32105c50c794f0155080bc1e15bafc1180c8f28
Shayan Golafshani <shayangolafsh@gmail.com>
2021-05-20 15:28:47 -0600

    Clean up filterRecipebyIngredientsMethod()

--- src/classes/RecipeRepository.js
+++ src/classes/RecipeRepository.js
@@ -47,49 +47,14 @@ class RecipeRepository {
   }
 
   filterRecipeByIngredients(userInput) {  
-    //ingredients.forEach((ingredien))
-
-    //we needd to return an array of the recipes 
-    //that contain the ingredient that the user-inputted
-    //to do that we need to filter through all the recipes
-    //and return those which contain the ingredient
-
-    //to do that we need to match up the id in the ingredients array
-    // then check the ingredient.name to see if it includes() the ingredient we passed in.
-    // this.recipes.filter((recipe) => {
-    //   recipe.ingredients.forEach((ingredient) => {
-    //       if(ingredient.id === ingredientsData.id)
-    //   })
-    // });
-
-    //test the booleans on line 70
-// refactor this with the foreach on ingredients first the 
-    let recipesWithMatchIngreds = [];
-  //   ingredientsData.forEach(data => {
-  //     recipesWithMatchIngreds = this.recipes.filter(recipe => { 
-  //       recipe.ingredients.find(ingredient => {
-  //       return ingredient.id === data.id && data.name.includes(userInput)
-  //     })
-  //   })
-  // })
-  // }
-
-
-    this.recipes.map((recipe) => {
-      ingredientsData.forEach((data) => {
-        recipe.ingredients.forEach((ingredient) => {
-                //console.log(ingredient);
-                //console.log(data);
-               if (ingredient.id === data.id && data.name.includes(userInput)) {
-            recipesWithMatchIngreds.push(recipe);
-          }
+    let matchingRecipes = [];
+    ingredientsData.forEach(data => {
+      matchingRecipes = this.recipes.filter(recipe => { 
+        recipe.ingredients.find(ingredient => {
+          return ingredient.id === data.id && data.name.includes(userInput)
         });
-        
-        //ingredientNames.push(data.name);
-
-      })
-    })
-
+      });
+    });
   }
 }
     
