a34dcb39c8a8d8179dff5427fa1a67e09452bbc8
Shayan Golafshani <shayangolafsh@gmail.com>
2021-05-20 15:06:07 -0600

    Start user class

---
+++ src/classes/User.js
@@ -0,0 +1,16 @@
+class User {
+  constructor(userDetails) {
+    this.name = userDetails.name;
+    this.id = userDetails.id;
+    this.pantry = userDetails.pantry;
+    this.favoriteRecipes = [];
+    this.recipesToCook = [];
+  }
+
+  
+
+
+
+}
+
+export default User;
