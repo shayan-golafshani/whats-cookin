2930460edef1dc4282a9c9167bc92afa476b83e8
Shayan Golafshani <shayangolafsh@gmail.com>
2021-05-20 20:47:08 -0600

    Add implementation for all of User.js

--- src/classes/User.js
+++ src/classes/User.js
@@ -1,3 +1,5 @@
+import RecipeRepository from "./RecipeRepository";
+
 class User {
   constructor(userDetails) {
     this.name = userDetails.name;
@@ -7,9 +9,54 @@ class User {
     this.recipesToCook = [];
   }
 
-  
+  addFromFavorites(recipe) {
+  //is this already in the users favorites  if so, ignore it
+  //otherwise push it into the array.
+    if (!this.favoriteRecipes.includes(recipe)) {
+      this.favoriteRecipes.push(recipe);
+    } else {
+      console.log('sorry bud, you already favorited that');
+    }
+  }
 
+  removeFromFavorites(recipe) {
+    if (this.favoriteRecipes.includes(recipe)) {
+      this.favoriteRecipes.splice(this.favoriteRecipes.indexOf(recipe), 1);
+    } else {
+      console.log('sorry but that recipe ain\'t here no more');
+    }
+  }
 
+  addToRecipesToCook(recipe) {
+    if (!this.favoriteRecipes.includes(recipe)) {
+      this.favoriteRecipes.push(recipe);
+    } else {
+      console.log('sorry bud, you already favorited that');
+    }
+  }
+
+  removeFromRecipesToCook(recipe) {
+    if (this.favoriteRecipes.includes(recipe)) {
+      this.favoriteRecipes.splice(this.favoriteRecipes.indexOf(recipe), 1);
+    } else {
+      console.log('sorry but that recipe ain\'t here no more');
+    }
+  }
+
+  filterFavoriteRecipesByTags(mealTags) {
+    let filterFavsByTag = new RecipeRepository(this.favoriteRecipes);
+    return filterFavsByTag.filterByTags(mealTags);
+  }
+
+  filterFavoriteRecipesByName(name) {
+    let filterFavsByName = new RecipeRepository(this.favoriteRecipes);
+    return filterFavsByName.filterByName(name);    
+  }
+
+  filterFavoriteRecipesByIngreds(ingreds) {
+    let filterFavsByIngreds = new RecipeRepository(this.favoriteRecipes);
+    return filterFavsByIngreds.filterRecipeByIngredients(ingreds);
+  }
 
 }
 
