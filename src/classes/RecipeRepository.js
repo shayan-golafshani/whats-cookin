22feb9e959623520a394ce1273246dbf91629bea
Shayan Golafshani <shayangolafsh@gmail.com>
2021-05-21 20:24:08 -0600

    Cleaned up reciperepo even more

--- src/classes/RecipeRepository.js
+++ src/classes/RecipeRepository.js
@@ -5,45 +5,15 @@ class RecipeRepository {
     this.recipes = data;
   }
 
-  // filterByTags(...searchTags) {
-  //   let mealTags = searchTags.map(tag => tag.toLowerCase())
-  //   console.log(mealTags) 
-  //   let filteredRecipes = [];
-  //   this.recipes.forEach((recipe) => {
-  //     recipe.tags.filter((tag) => {
-  //       if (mealTags.includes(tag)) {
-  //         filteredRecipes.push(recipe);
-  //       }
-  //     })
-  //   });
-  //   return filteredRecipes;
-  // }
-
   filterByTags(...searchTags) {
-    //may not need to lowercase the taggies
     let mealTags = searchTags.map(tag => tag.toLowerCase());
-    return this.recipes.filter(recipe => recipe.tags.some(tag => mealTags.includes(tag)))
+    return this.recipes.filter(
+      recipe => recipe.tags.some(tag => mealTags.includes(tag)))
   }
 
-  // retrieveRecipesByTag(...tags) {
-  //   const lowerCaseTags = tags.map(tag => tag.toLowerCase());
-  //   let results =  lowerCaseTags.reduce((matchingRecipes, tag) => {
-  //     this.recipesData.forEach(recipe => {
-  //       if (recipe.tags.includes(tag)) {
-  //         matchingRecipes.push(recipe);
-  //       }
-  //     })
-  //     return matchingRecipes
-  //   }, [])
-  //   return results
-  // }
-
   filterByName(name) {
-    let lowerCaseRecipeName = name.toLowerCase();
-    return this.recipes.filter((recipe) => {
-      let lowerCaseName = recipe.name.toLowerCase()
-      return lowerCaseName.includes(lowerCaseRecipeName);
-    });
+    return this.recipes.filter(
+      recipe => recipe.name.toLowerCase().includes(name.toLowerCase()))
   }
 
   filterRecipeByIngredients(userInput) {  
