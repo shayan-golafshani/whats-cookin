b43ea7d381a83956a2735cce32c66da7a056c34e
Shayan Golafshani <shayangolafsh@gmail.com>
2021-05-21 20:10:31 -0600

    Refactor filterbytags

--- src/classes/RecipeRepository.js
+++ src/classes/RecipeRepository.js
@@ -5,17 +5,38 @@ class RecipeRepository {
     this.recipes = data;
   }
 
-  filterByTags(mealTags) { 
-    let filteredRecipes = [];
-    this.recipes.forEach((recipe) => {
-      recipe.tags.filter((tag) => {
-        if (mealTags.includes(tag)) {
-          filteredRecipes.push(recipe);
-        }
-      })
-    });
-    return filteredRecipes;
+  // filterByTags(...searchTags) {
+  //   let mealTags = searchTags.map(tag => tag.toLowerCase())
+  //   console.log(mealTags) 
+  //   let filteredRecipes = [];
+  //   this.recipes.forEach((recipe) => {
+  //     recipe.tags.filter((tag) => {
+  //       if (mealTags.includes(tag)) {
+  //         filteredRecipes.push(recipe);
+  //       }
+  //     })
+  //   });
+  //   return filteredRecipes;
+  // }
+
+  filterByTags(...searchTags) {
+    //may not need to lowercase the taggies
+    let mealTags = searchTags.map(tag => tag.toLowerCase());
+    return this.recipes.filter(recipe => recipe.tags.some(tag => mealTags.includes(tag)))
   }
+
+  // retrieveRecipesByTag(...tags) {
+  //   const lowerCaseTags = tags.map(tag => tag.toLowerCase());
+  //   let results =  lowerCaseTags.reduce((matchingRecipes, tag) => {
+  //     this.recipesData.forEach(recipe => {
+  //       if (recipe.tags.includes(tag)) {
+  //         matchingRecipes.push(recipe);
+  //       }
+  //     })
+  //     return matchingRecipes
+  //   }, [])
+  //   return results
+  // }
 
   filterByName(name) {
     let lowerCaseRecipeName = name.toLowerCase();
