ffcdbcc7d14297b09839c2c52ff59696a4ee313c
Shayan Golafshani <shayangolafsh@gmail.com>
2021-05-23 20:43:32 -0600

    Clean up

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,10 +1,7 @@
-// Your fetch requests will live here!
-
 let userUrl = 'http://localhost:3001/api/v1/users';
 let ingredUrl = 'http://localhost:3001/api/v1/ingredients';
 let recipeUrl = 'http://localhost:3001/api/v1/recipes'
 
-//GET ALL USERS
 const apiUsers = () => fetch(userUrl)
   .then( response => response.json() )
   .then( data => console.log(data) )
@@ -22,6 +19,6 @@ const apiRecipes = () => fetch(recipeUrl)
 .catch( err => console.log(err));
 
 
-const getAllData () => { Promise.all([apiUsers(), apiIngreds(), apiRecipes()]) }
+const getAllData = () => Promise.all([apiUsers(), apiIngreds(), apiRecipes()]) 
 
 export default { apiUsers, apiIngreds, apiRecipes, getAllData}
