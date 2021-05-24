a2a8d58937ebcd300654533a697915aaed3fb95d
Shayan Golafshani <shayangolafsh@gmail.com>
2021-05-23 20:27:54 -0600

    Start apiCalls

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,4 +1,30 @@
 // Your fetch requests will live here!
 
+let userUrl = 'http://localhost:3001/api/v1/users';
+let ingredUrl = 'http://localhost:3001/api/v1/ingredients';
+let recipeUrl = 'http://localhost:3001/api/v1/recipes'
 
-console.log('I will be a fetch request!')
+//GET ALL USERS
+const apiUsers = fetch(userUrl)
+  .then( response => response.json() )
+  .then( data => console.log(data) )
+  .catch( err => console.log(err) );
+
+const apiIngreds = fetch(ingredUrl)
+.then( response => response.json() )
+.then( data => console.log(data) )
+.catch( err => console.log(err) );
+
+
+const apiRecipes = fetch(recipeUrl)
+.then( response => response.json() )
+.then( data => console.log(data))
+.catch( err => console.log(err));
+//GET ALL INGREDIENTS
+//http://localhost:3001/api/v1/ingredients
+
+//Get all recipes
+//http://localhost:3001/api/v1/recipes
+
+
+//console.log('I will be a fetch request!')
