b109bfebef214f3ac02e760c61c189a62d2a29e3
Shayan Golafshani <shayangolafsh@gmail.com>
2021-05-23 20:41:11 -0600

    Use promise.all to make sure that everything happens together

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -5,21 +5,24 @@ let ingredUrl = 'http://localhost:3001/api/v1/ingredients';
 let recipeUrl = 'http://localhost:3001/api/v1/recipes'
 
 //GET ALL USERS
-const apiUsers = fetch(userUrl)
+const apiUsers = () => fetch(userUrl)
   .then( response => response.json() )
   .then( data => console.log(data) )
   .catch( err => console.log(err) );
 
-const apiIngreds = fetch(ingredUrl)
+const apiIngreds = () => fetch(ingredUrl)
 .then( response => response.json() )
 .then( data => console.log(data) )
 .catch( err => console.log(err) );
 
 
-const apiRecipes = fetch(recipeUrl)
+const apiRecipes = () => fetch(recipeUrl)
 .then( response => response.json() )
 .then( data => console.log(data))
 .catch( err => console.log(err));
+
+
+const getAllData () => { Promise.all([apiUsers(), apiIngreds(), apiRecipes()]) }
 //GET ALL INGREDIENTS
 //http://localhost:3001/api/v1/ingredients
 
